c55fd5e551e100a34ca008c8e195112e33d85900
Plasencia2006 <plasenciaalvahectorisai@gmail.com>
2025-01-29 19:09:32 -0500

    Actualizar pagina

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,5 @@ import { Router } from "express";
 const router = Router();
 router.get('/', (req, res) =>res.render('index.ejs', {title: 'Venta productos'}));
 router.get('/login', (req, res) =>res.render('login.ejs', {title: 'Login'}));
-
+router.get('/contactanos', (req, res) =>res.render('contactanos.ejs', {title: 'Register'}));
 export default router;
