825ab4fe40f29f7d039eb50aff55435a07ce4240
Plasencia2006 <plasenciaalvahectorisai@gmail.com>
2025-01-30 18:43:19 -0500

    actualizar portafolio

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 router.get('/', (req, res) =>res.render('index.ejs', {title: 'Bienvenido'}));
-router.get('/login', (req, res) =>res.render('login.ejs', {title: 'Registro'}));
-router.get('/contactanos', (req, res) =>res.render('contactanos.ejs', {title: 'contactame'}));
+router.get('/servicios', (req, res) => res.render('servicios.ejs', { title: 'Nuestros servicios' }));
+router.get('/contacto', (req, res) =>res.render('contacto.ejs', {title: 'contactame'}));
+router.get('/asistente', (req, res) =>res.render('asistente.ejs', {title: 'Conversemos'}));
 router.get('/head', (req, res) =>res.render('head.ejs', {title: 'Registro'}));
 export default router;
